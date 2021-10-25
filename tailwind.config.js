56a83898ce6f9f37d78ad5bd0400b39186113ed1
Richard <richard.wigley@gmail.com>
2021-10-25 13:15:21 +0100

    Tailwind custom font size

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,21 @@ module.exports = {
         ]
       }
     },
+    fontSize: {
+      xs: ['0.6875rem', { lineHeight: '0.875rem' }],   // 11px
+      sm: ['0.8125rem', { lineHeight: '0.9375rem' }],  // 13px
+      base: ['0.875rem', { lineHeight: '1rem' }],      // 14px
+      lg: ['0.9375rem', { lineHeight: '1.5rem' }],     // 15px
+      xl: ['1.125rem', { lineHeight: '1.5rem' }],      // 18px
+      '2xl': ['1.5rem', { lineHeight: '2rem' }],
+      '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
+      '4xl': ['2.25rem', { lineHeight: '2.5rem' }],
+      '5xl': ['3rem', { lineHeight: '1' }],
+      '6xl': ['3.75rem', { lineHeight: '1' }],
+      '7xl': ['4.5rem', { lineHeight: '1' }],
+      '8xl': ['6rem', { lineHeight: '1' }],
+      '9xl': ['8rem', { lineHeight: '1' }],
+    },
   },
   variants: {
     extend: {},
