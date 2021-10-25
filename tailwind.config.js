ff652ef52c392bea4a2175afca27c33cdc4f5756
Richard <richard.wigley@gmail.com>
2021-10-25 12:24:38 +0100

    Tailwind custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+       primary: {
+         200: 'var(--color-primary-200)',
+       },
+       neutral: {
+         200: 'var(--color-neutral-200)',
+         300: 'var(--color-neutral-300)',
+         400: 'var(--color-neutral-400)',
+         500: 'var(--color-neutral-500)',
+         600: 'var(--color-neutral-600)',
+         700: 'var(--color-neutral-700)',
+       }
+      },
+    },
   },
   variants: {
     extend: {},
