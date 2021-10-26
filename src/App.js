9269503ec5dbf293dbe4a91e80bf03f94862e775
Richard <richard.wigley@gmail.com>
2021-10-26 18:33:17 +0100

    App has data from json

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import {useState} from "react";
 import FilterPanel from "./components/filterPanel";
 import JobCard from "./components/jobCard";
+import data from  "./data/data.json"
 
 function App() {
+  const [jobs] = useState(data);
   const filterList = ["Frontend", "Senior", "HTML", "JavaScript"]
-  const jobFilterList = ["Frontend", "Junior", "HTML", "JavaScript"]
 
   return (
     <div className="App">
@@ -12,50 +14,20 @@ function App() {
       <div className="mx-6 mb-9">
         <FilterPanel filterList={filterList}/>
 
-        <JobCard
-          company="FaceIt"
-          logo="/images/photosnap.svg"
-          knew={true}
-          featured={true}
-          position="Developer"
-          jobFilterList={jobFilterList}
-          postedAt="1d ago"
-          contract="Full Time"
-          location="Remote"
-        />
-        <JobCard
-          company="Manage"
-          logo="/images/manage.svg"
-          knew={true}
-          featured={false}
-          position="FullStack"
-          jobFilterList={jobFilterList}
-          postedAt="2w ago"
-          contract="Contract"
-          location="UK Only"
-        />
-        <JobCard
-          company="Insure"
-          logo="/images/account.svg"
-          knew={false}
-          featured={true}
-          position="Junior"
-          jobFilterList={jobFilterList}
-          postedAt="3w ago"
-          contract="Full Time"
-          location="USA Only"
-        />
-        <JobCard
-          company="MyHome"
-          logo="/images/myhome.svg"
-          knew={false}
-          featured={false}
-          position="Senior"
-          jobFilterList={jobFilterList}
-          postedAt="4w ago"
-          contract="Part Time"
-          location="Worldwide"
-        />
+        {jobs.map((job) =>
+          <JobCard
+            id={job.id}
+            company={job.company}
+            logo={job.logo}
+            knew={job.new}
+            featured={true}
+            position={job.position}
+            jobFilterList={[job.role, job.level, ...job.languages]}
+            postedAt={job.postedAt}
+            contract={job.contract}
+            location={job.location}
+          />
+        )}
       </div>
 
     </div>
