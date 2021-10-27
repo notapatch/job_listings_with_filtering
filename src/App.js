8472a9b6238d7d61b8e1de97fda63bf1fa025eba
Richard <richard.wigley@gmail.com>
2021-10-27 18:39:43 +0100

    filterPanel hidden if no filterList

--- src/App.js
+++ src/App.js
@@ -20,7 +20,9 @@ function App() {
       <header className="h-156 bg-primary-200 bg-header-mobile sm:bg-header-desktop" />
 
       <div className="mx-6 mb-9">
-        <FilterPanel filterList={filterList} handleRemoveFilter={handleRemoveFilter}/>
+        <div className={!filterList.length ? "hidden" : "" }>
+          <FilterPanel filterList={filterList} handleRemoveFilter={handleRemoveFilter}/>
+        </div>
 
         {jobs.map((job) =>
           <JobCard
